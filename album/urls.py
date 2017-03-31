437cf759e68fbbd5fd84f9229b1acd489e93a5d4
luis <alejandroarmero@hotmail.com>
2017-03-31 17:54:00 -0500

    Proyecto con bootstrap y formularios

--- album/urls.py
+++ album/urls.py
@@ -7,5 +7,11 @@ urlpatterns = [
     url(r'^category/(?P<category_id>[0-9])/detail/$', views.category_detail, name="category-detail"),
     url(r'^photo/$', views.PhotoListView.as_view(), name="photo-list"),
     url(r'^photo/(?P<pk>[0-9]+)/detail/$', views.PhotoDetailView.as_view(), name="photo-detail"),
+    # Update
+	url(r'^photo/(?P<pk>[0-9]+)/update/$', views.PhotoUpdate.as_view(),name='photo-update'),
+	#Create
+	url(r'^photo/create/$', views.PhotoCreate.as_view(),name='photo-create'),
+	#Delete
+	url(r'^photo/(?P<pk>[0-9]+)/delete/$', views.PhotoDelete.as_view(),name='photo-delete'),  
 
 ]
--- album/views.py
+++ album/views.py
@@ -2,9 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from album.models import Category,Photo #a  importar category y Photo para las clases
 from django.views.generic import ListView, DetailView # a
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic.edit import UpdateView, CreateView, DeleteView
 # Create your views here.
+
+class PhotoUpdate(UpdateView):
+	model = Photo
+	fields = '__all__'
+
+
+class PhotoCreate(CreateView):
+	model=Photo
+	fields = '__all__'
+
+
+class PhotoDelete(DeleteView):
+	model=Photo
+	success_url=reverse_lazy('photo-list')
+
 def first_view(request):
-	return HttpResponse("<h1>Esta es mi primer vista</h1>")
+	return render(request,'base.html')
 
 def category(request):
 	category_list=Category.objects.all() #Guarda el contenido del modelo -> Category
